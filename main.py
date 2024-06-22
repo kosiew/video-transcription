2bb83ff3e96a4318c56c15d8075fec66fcdeee51
Siew Kam Onn <kosiew@gmail.com>
2024-06-22 13:27:36 +0800

    shorten srt filename

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 import subprocess
 from datetime import timedelta
 from typing import Annotated
@@ -8,6 +9,11 @@ from rich import print
 from typer import Argument
 
 app = typer.Typer()
+
+# sample srt file name Harry Wild - S01E02 - Samurai Plague Doctor Kills for Kicks.srt
+# regex pattern to extract the season and episode number
+pattern = r"S(\d{2})E(\d{2})"
+regex = re.compile(pattern)
 
 
 @app.command()
@@ -28,11 +34,12 @@ def transcribe_wav(
 ) -> str:
     whisper = "/Users/kosiew/github/whisper.cpp/main  -m /Users/kosiew/GitHub/whisper.cpp/models/ggml-large-v2.bin"
     whisper_command = f'{whisper} -osrt -f "{wav_file}"'
-    print(f"Transcribing audio from {wav_file}")
-    subprocess.run(whisper_command, shell=True)
     wav_srt_file = f"{wav_file}.srt"
-    # rename to remove .wav
     srt_file = wav_srt_file.replace(".wav", "")
+    srt_file = shorten_srt_filename(srt_file)
+    
+    print(f"Transcribing audio from {wav_file} to {srt_file}")
+    subprocess.run(whisper_command, shell=True)
     # os rename the file
     os.rename(wav_srt_file, srt_file)
     return srt_file
@@ -63,16 +70,57 @@ def add_srt_to_video(input_video, subtitles_file, output_file):
     transcribe_video(input_video_path)
     add_srt_to_video(input_video_path, output_file)
 
+def shorten_srt_filename(srt_file: str) -> str:
+    # eg shorten /Volumes/F/Movies/Harry Wild/Season 1/Harry Wild - S01E02 - Samurai Plague Doctor Kills for Kicks.srt
+    # to /Volumes/F/Movies/Harry Wild/Season 1/S01E02.srt
+    global regex
+    match = regex.search(srt_file)
+    
+    short_filename = srt_file
+    
+    # use regex replace
+    if match:
+        season = match.group(1)
+        episode = match.group(2)
+        _short_filename = f"S{season}E{episode}.srt"
+        # join folder to short_filename
+        folder = os.path.dirname(srt_file)
+        short_filename = os.path.join(folder, _short_filename)
+        
+    return short_filename
+
+@app.command()
+def rename_to_short_srt_filename_in_folder(
+    folder: Annotated[str, Argument(help="The folder the srt files")]
+):
+    file_count = 0
+    for file in os.listdir(folder):
+        if file.endswith(".srt"):
+            rename_to_short_srt_filename(os.path.join(folder, file))
+            file_count += 1
+    print(f"Renamed {file_count} files")
+    
+
+@app.command()
+def rename_to_short_srt_filename(
+    srt_file: Annotated[str, Argument(help="The path to the srt file")]
+):
+    short_filename = shorten_srt_filename(srt_file)
+    if short_filename != srt_file:
+        os.rename(srt_file, short_filename)
+        print(f"Renamed {srt_file} to {short_filename}")    
 
 @app.command()
 def transcribe_folder(
     folder: Annotated[str, Argument(help="The path to the folder with video files")]
 ):
+    file_count = 0
     for file in os.listdir(folder):
         if file.endswith(".mp4") or file.endswith(".mkv"):
             srt_file = transcribe_video(os.path.join(folder, file))
             print(f"Transcribed {file} to {srt_file}")
-
+            file_count += 1
+    print(f"Transcribed {file_count} files")
 
 if __name__ == "__main__":
     app()
